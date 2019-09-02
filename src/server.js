0953a435ab37edb12356a07382f6f8f6abc48ea3
Casey Benko <casey.benko@bltgv.com>
2019-09-02 16:28:52 -0400

    change basepath

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const dev = NODE_ENV === 'development';
 
 polka() // You can also use Express
 	.use(
+		'/blt-main-svelte-concept',
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
